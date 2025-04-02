04a170b6966c9e891b870045a6b1cd728a75c1a3
zenmanphd <git@stackblitz.com>
2025-04-02 14:53:10 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>We have now gained control. Operation Golden Child is in effect.</h1>
+        <br/>
+        <br/>
+        <p>
+          Je suis plu sage cu je parait
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
